1a7af301c0568179b593b66d8fd970d0fdb4ed31
Fernirx <anhtuhungdeveloper@gmail.com>
2025-03-15 10:50:41 +0700

    fix database connection

--- src/app/config/database.py
+++ src/app/config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+mysqlconnector://ujnelffy_cnpmadmin%40CNPM1!@localhost:3306/ujnelffy_flight_booking"
+DATABASE_URL = "mysql+mysqlconnector://ujnelffy_cnpm:admin%40CNPM1!@103.15.222.22:3306/ujnelffy_flight_booking"
 Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
